4b95885caf51e5ed019f2b965cdf1c85f65bf95c
MatheusKristman <kristman058@gmail.com>
2024-04-17 21:34:41 -0300

    testing convex

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -8,17 +8,17 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
-    children,
+  children,
 }: {
-    children: ReactNode;
+  children: ReactNode;
 }) {
-    return (
-        <ClerkProvider
-            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-        >
-            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-                {children}
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    );
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,47 @@
 "use client";
 
 import {
-    SignInButton,
-    SignOutButton,
-    SignedIn,
-    SignedOut,
-    useSession,
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  useSession,
 } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 
 export default function Home() {
-    const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
 
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <SignedIn>
-                <SignOutButton>
-                    <Button>Sign Out</Button>
-                </SignOutButton>
-            </SignedIn>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
 
-            <SignedOut>
-                <SignInButton mode="modal">
-                    <Button>Sign In</Button>
-                </SignInButton>
-            </SignedOut>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
 
-            <Button
-                onClick={() => {
-                    createFile({
-                        name: "hello world",
-                    });
-                }}
-            >
-                Click Me
-            </Button>
-        </main>
-    );
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+      <Button
+        onClick={() => {
+          createFile({
+            name: "hello world",
+          });
+        }}
+      >
+        Click Me
+      </Button>
+    </main>
+  );
 }
